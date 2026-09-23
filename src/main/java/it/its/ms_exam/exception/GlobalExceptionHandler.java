09c0041cad3caa68503f2e07d10099e06e231c8d
Andrea Gennaro <gennaro.andrea1998@gmail.com>
2026-09-23 20:19:21 +0200

    ...

---
+++ src/main/java/it/its/ms_exam/exception/GlobalExceptionHandler.java
@@ -0,0 +1,10 @@
+package it.its.ms_exam.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    
+
+}
